4784158eeb33d4964d903a0923983bffae74ee3d
kaun <kuan@gmail.com>
2026-02-16 17:59:39 +0800

    前端第一版卡片

--- v0/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AttractionCarousel } from "@/components/attraction-carousel"
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-background">
-      <AttractionCarousel />
-    </main>
-  )
-}
